4d73037a7fb3e54464693dc4f6bb6b0de68ae671
s33dco <code@s33d.co>
2019-06-05 18:09:52 +0100

    icons on welcome page

--- src/components/WelcomePage.js
+++ src/components/WelcomePage.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import Icon from './Icon'
 
 const WelcomePage = () => (
 	<main>
@@ -10,6 +11,17 @@ const WelcomePage = () => (
 			<p>Front end, Back end, Full Stack, JAMStack.</p>
 			<p>Single page apps, RESTful APIS, web sites and applications.</p>
 			<p>Rails and MERN.</p>
+			<div className='techbox'>
+				<Icon className='techbox__icon' imageLink={`./images/ruby.png`} />
+				<Icon className='techbox__icon' imageLink={`./images/rails.png`} />
+				<Icon className='techbox__icon' imageLink={`./images/node.png`} />
+				<Icon className='techbox__icon' imageLink={`./images/js.png`} />
+				<Icon className='techbox__icon' imageLink={`./images/mongo.png`} />
+				<Icon className='techbox__icon' imageLink={`./images/express.png`} />
+				<Icon className='techbox__icon' imageLink={`./images/react.png`} />
+				<Icon className='techbox__icon' imageLink={`./images/redux.png`} />
+				<Icon className='techbox__icon' imageLink={`./images/webpack.png`} />
+			</div>
 		</div>
 	</main>
 )
